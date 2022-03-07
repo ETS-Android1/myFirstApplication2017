36adb79c79e9022f6aa455b3e3f31607ad668c13
shilo saadon <49208325+shiloSaadon@users.noreply.github.com>
2022-03-07 15:55:11 +0200

    Update Config.java

--- app/src/main/java/run/example/shilo/finalprogect/Config.java
+++ app/src/main/java/run/example/shilo/finalprogect/Config.java
@@ -3,7 +3,7 @@ package run.example.shilo.finalprogect;
  * Created by Belal on 10/30/2015.
  */
 public class Config {
-    public static final String EMAIL ="simplecode1002@gmail.com";
-    public static final String PASSWORD ="simplecode050";
-    public static final String EMAILTOGET = "simplecode1002@gmail.com";
+    public static final String EMAIL =""; // put your email
+    public static final String PASSWORD =""; // put your password
+    public static final String EMAILTOGET = ""; //put your email
 }
